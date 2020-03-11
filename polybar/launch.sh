08b8f2b2d75425601ec619350cecc3b5c28b02d9
Martin Asquino <martin.asquino@gmail.com>
2020-03-11 16:03:03 +0000

    more stuff

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+m=$(xrandr --query | grep " connected" | grep primary | cut -d" " -f1)
+cmd=(env "MONITOR=$m" polybar --reload main)
 
-echo "Polybar launched..."
+if [[ $# -gt 0 ]] && [[ $1 = "block" ]]; then
+	exec "${cmd[@]}"
+else
+	"${cmd[@]}" &
+fi
+
