2dbf32a7102b945e0c6242e975307389c7c189e3
Martin Asquino <martin.asquino@gmail.com>
2023-01-20 16:58:37 +0000

    move from packer to lazy

--- vim/init.lua
+++ vim/init.lua
@@ -1,13 +1,16 @@
-_G.__luacache_config = {
-  chunks = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_chunks',
-  },
-  modpaths = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_modpaths',
-  }
-}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require('impatient')
 
 require('partials/plugins')
--- vim/lua/partials/plugins.lua
+++ vim/lua/partials/plugins.lua
@@ -1,63 +1,51 @@
-_G.__luacache_config = {
-  chunks = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_chunks',
+require("lazy").setup({
+  'mhartington/formatter.nvim',
+  'lewis6991/impatient.nvim',
+  'dstein64/vim-startuptime',
+  'github/copilot.vim',
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "c", "lua", "rust", "go", "vim" },
+        highlight = { enable = true, }
+      }
+    end
   },
-  modpaths = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_modpaths',
-  }
-}
-require('impatient')
-
-require('packer').startup(function(use)
-  use { 'wbthomason/packer.nvim' }
-
-  use { 'mhartington/formatter.nvim' }
-  use { 'lewis6991/impatient.nvim' }
-  use { 'dstein64/vim-startuptime' }
-  use { 'github/copilot.vim' }
-  use { 'nvim-treesitter/nvim-treesitter' }
-  use { 'nvim-treesitter/playground' }
-  use { 'ibhagwan/fzf-lua' }
-  use { 'vim-test/vim-test', ft = { 'go', 'rust' } }
-  use { 'tpope/vim-abolish' }
-  use { 'tpope/vim-surround' }
-  use { 'tpope/vim-commentary' }
-  use { 'tpope/vim-repeat' }
-  use { 'tpope/vim-projectionist' }
-  use { 'tpope/vim-fugitive' }
-  use { 'neovim/nvim-lspconfig', config = function()
+  'ibhagwan/fzf-lua',
+  { 'vim-test/vim-test', ft = { 'go', 'rust' }},
+  'tpope/vim-abolish',
+  'tpope/vim-surround',
+  'tpope/vim-commentary',
+  'tpope/vim-repeat',
+  'tpope/vim-projectionist',
+  'tpope/vim-fugitive',
+  { 'neovim/nvim-lspconfig', config = function()
     vim.api.nvim_exec([[
       hi LspReferenceText guibg=grey guifg=yellow
     ]], false)
-  end }
-  use { 'nvim-tree/nvim-tree.lua',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-  }
-  use { 'hrsh7th/vim-vsnip-integ' }
-  use { 'hrsh7th/vim-vsnip' }
-  use { 'hrsh7th/nvim-cmp',
-    requires = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/cmp-buffer',
-    }
-  }
-  use { 'schickling/vim-bufonly' }
-  use { 'majutsushi/tagbar' }
-  use { 'airblade/vim-rooter' }
-  use { 'rhysd/vim-go-impl', ft = { 'go' } }
-  use { 'martskins/vim-cargo-search', ft = { 'rust' } }
-  use { 'jparise/vim-graphql', ft = { 'graphql' } }
-  use { 'cespare/vim-toml', ft = { 'toml' } }
-  use { 'tyru/open-browser-github.vim', requires = { 'tyru/open-browser.vim' } }
-  use { 'ellisonleao/gruvbox.nvim', config = function()
+  end },
+  'nvim-tree/nvim-tree.lua',
+    'nvim-tree/nvim-web-devicons',
+  'hrsh7th/vim-vsnip-integ',
+  'hrsh7th/vim-vsnip',
+  'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/cmp-buffer',
+  'schickling/vim-bufonly',
+  'majutsushi/tagbar',
+  'airblade/vim-rooter',
+  { 'johejo/gomod.vim', ft = { 'gomod' }},
+  { 'rhysd/vim-go-impl', ft = { 'go' }},
+  { 'martskins/vim-cargo-search', ft = { 'rust' }},
+  { 'jparise/vim-graphql', ft = { 'graphql' }},
+  { 'cespare/vim-toml', ft = { 'toml' }},
+  { 'ellisonleao/gruvbox.nvim', lazy = false, config = function()
     require('gruvbox').setup({
       italic = false
     })
     require('gruvbox').load()
-  end }
-  use { 'johejo/gomod.vim' }
-end)
+  end },
+})
