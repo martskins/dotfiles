5d33785aae160090cf865f5aa38dc572ef69173b
Martin Asquino <martin.asquino@gmail.com>
2023-02-07 12:40:37 +0000

    tagbar tweaks

--- vim/lua/partials/tagbar.lua
+++ vim/lua/partials/tagbar.lua
@@ -2,13 +2,15 @@ local module = {}
 
 function module.config()
   local g = vim.g
-  g.tagbar_foldlevel = 0
+  g.tagbar_autofocus = 1
+  g.tagbar_compact = 1
+  g.tagbar_expand = 1
   g.tagbar_sort = 0
 end
 
 function module.init()
   local map = vim.api.nvim_set_keymap
-  map('n', ',.', ':TagbarToggle<CR><C-w><C-w>', { silent = true })
+  map('n', ',.', ':TagbarToggle<CR>', { silent = true })
 end
 
 return module
